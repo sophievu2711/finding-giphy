e88f47ff1c873d4dbaffec5835d2c2b0cae68a5a
Nga Vu <svu2711@gmail.com>
2020-08-15 13:28:44 +1000

    Updates

---
+++ precache-manifest.6af4a9f205a4c6206010fd8c8960e441.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de65bfd27dcf04d1308df400d9c376f",
+    "url": "/finding-giphy/index.html"
+  },
+  {
+    "revision": "a51e953cfbc496a12e7f",
+    "url": "/finding-giphy/static/css/main.8bec9126.chunk.css"
+  },
+  {
+    "revision": "8af3e4f18a4538cdc064",
+    "url": "/finding-giphy/static/js/2.77c29736.chunk.js"
+  },
+  {
+    "revision": "a51e953cfbc496a12e7f",
+    "url": "/finding-giphy/static/js/main.a976aae9.chunk.js"
+  },
+  {
+    "revision": "39b957755a6a56c608bb",
+    "url": "/finding-giphy/static/js/runtime-main.4b74abe1.js"
+  }
+]);
